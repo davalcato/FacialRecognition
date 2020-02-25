c900e450f26e8e4999a3a392ab4fad0397801369
davalcato <davalcato@yahoo.com>
2020-02-24 20:03:52 -0800

    first commit

---
+++ FacialRecognition/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FacialRecognition
+//
+//  Created by Daval Cato on 2/24/20.
+//  Copyright © 2020 Daval Cato. All rights reserved.
+//
+
+import UIKit
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate,  {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
