1565486e62e3a3b86c7641d30123a5ece0b1c5ce
davalcato <davalcato@yahoo.com>
2020-02-27 18:48:50 -0800

    added the IBOutlet for the imageView, activityIndicator, fadeView, and the findFacesButton in the ViewController

--- FacialRecognition/ViewController.swift
+++ FacialRecognition/ViewController.swift
@@ -9,8 +9,21 @@
 import UIKit
 import Vision
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate,  {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var fadeView: UIView!
+    
+    @IBOutlet weak var findFacesButton: UIButton!
+    
+    var picker:UIImagePickerController!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
